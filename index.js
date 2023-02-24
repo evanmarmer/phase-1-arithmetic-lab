d2fdf57456b27bf3e20c2516a9562bb6f9f09f3b
evanmarmer <evanmarmer@gmail.com>
2023-02-24 16:51:59 -0700

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 19
+let num4 = 5
+let mod = 19 % 5
+
+let max = Math.max (5,10,20)
